0ebd2783eed84e5132f0319e2877362080dc225c
AmosAidoo <aidooamos20@gmail.com>
2019-06-05 12:47:04 +0000

    I've renamed the main class to FindRemainder

--- practice/beginner/FindRemainder.java
+++ practice/beginner/FindRemainder.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
-public class Main {
+public class FindRemainder {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t, n, a, b;
-        String input, a_b[];
-        t = scan.nextInt();
-        scan.nextLine();
-        while(t-- > 0){
-            input = scan.nextLine();
-            a_b = input.split(" ");
-            a = Integer.parseInt(a_b[0]);
-            b = Integer.parseInt(a_b[1]);
-            
-            System.out.println(rem(a, b));
+        try (Scanner scan = new Scanner(System.in)){
+            int t, n, a, b;
+            String input, a_b[];
+            t = scan.nextInt();
+            scan.nextLine();
+            while(t-- > 0){
+                input = scan.nextLine();
+                a_b = input.split(" ");
+                a = Integer.parseInt(a_b[0]);
+                b = Integer.parseInt(a_b[1]);
+                
+                System.out.println(rem(a, b));
+            }
         }
     }
     
