c450cf30cc5e57be2a4c5841d9b13d7baccc1286
AmosAidoo <aidooamos20@gmail.com>
2019-06-12 12:25:55 +0000

    I've moved all the files into the java directory

--- practice/beginner/NumberMirror.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class NumberMirror {
-    public static void main(String[] args){
-        try (Scanner scan = new Scanner(System.in)){
-            int n = scan.nextInt();
-            System.out.println(n);
-        }
-    }
-}
