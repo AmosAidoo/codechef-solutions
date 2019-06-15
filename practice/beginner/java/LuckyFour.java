99fb2dc35b32b9dc9598d37e0a4617becedf030f
AmosAidoo <aidooamos20@gmail.com>
2019-06-15 17:21:35 +0000

    Lucky Four

---
+++ practice/beginner/java/LuckyFour.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0){
+            System.out.println(br.readLine().replaceAll("[^4]", "").length());
+        }
+        br.close();
+    }
+}
