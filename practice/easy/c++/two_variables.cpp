cf8dbf8efe3e2f14df64951b8fdfb93ff1e02f04
AmosAidoo <aidooamos20@gmail.com>
2019-07-21 20:34:51 +0000

    Two Variables

---
+++ practice/easy/c++/two_variables.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	vector<ll> pws;
+	ll y = 0L;	
+	for (long long p = 0; p <= 1e9; p++){
+		long long product = p * p;
+		if (product > y){
+			y += product;
+			pws.push_back(p);
+		}
+	}
+	while (t--){
+		int xf;
+		cin >> xf;
+		
+		ll ans = upper_bound(pws.begin(), pws.end(), xf) - pws.begin();
+		cout << ans << "\n";
+	}
+}
